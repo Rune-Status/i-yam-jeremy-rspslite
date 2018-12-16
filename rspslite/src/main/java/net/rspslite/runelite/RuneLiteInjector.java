f4def3cc41a116b569ddbc6a2df88ff69b388e62
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2018-12-16 11:58:54 -0800

    Fixed launcher to use http over https
    For bootstrap.json

--- rspslite/src/main/java/net/rspslite/runelite/RuneLiteInjector.java
+++ rspslite/src/main/java/net/rspslite/runelite/RuneLiteInjector.java
@@ -3,7 +3,15 @@ package net.rspslite.runelite;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.function.Consumer;
+import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtField;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+import javassist.expr.NewExpr;
+import javassist.NotFoundException;
+import javassist.CannotCompileException;
 
 public class RuneLiteInjector {
 
@@ -11,7 +19,46 @@ public class RuneLiteInjector {
     Map<String, Consumer<CtClass>> injectors = new HashMap<>();
 
     injectors.put("net.runelite.launcher.Launcher", (cc) -> {
-      System.out.println("Injector called on " + cc.getName());
+      try {
+        ClassPool cp = cc.getClassPool();
+
+        // Disable https for download bootstrap.json
+        CtMethod getBootstrap = cc.getDeclaredMethod("getBootstrap", new CtClass[]{});
+        getBootstrap.instrument(new ExprEditor() {
+
+          @Override
+          public void edit(NewExpr expr) throws CannotCompileException {
+            if (expr.getClassName().equals("java.net.URL")) {
+              System.out.println("Fixed URL to use http instead of https");
+              expr.replace("$_ = new java.net.URL($proceed($$).toString().replace(\"https://\", \"http://\"));");
+            }
+          }
+
+        });
+
+
+        // Disable artifact JAR verification
+        CtMethod verifyJarHashes = cc.getDeclaredMethod("verifyJarHashes", new CtClass[]{cp.get("net.runelite.launcher.beans.Artifact[]")});
+        verifyJarHashes.insertBefore("return;");
+
+        // Replace RuneLite client with injected Runelite client
+        CtMethod download = cc.getDeclaredMethod("download", new CtClass[]{cp.get("net.runelite.launcher.LauncherFrame"), cp.get("net.runelite.launcher.beans.Bootstrap")});
+        download.instrument(new ExprEditor() {
+
+            @Override
+            public void edit(MethodCall m) throws CannotCompileException {
+              if (m.getMethodName().equals("getPath")) {
+                System.out.println("Replace getPath()");
+                m.replace("String path = $proceed($$); $_ = path.startsWith(\"http://repo.runelite.net/net/runelite/client/\") ? \"TODO\" : path;");
+              }
+            }
+
+        });
+
+      } catch (NotFoundException | CannotCompileException e) {
+        System.err.println("Unable to apply injector to " + cc.getName() + ". Skipping");
+        e.printStackTrace();
+      }
     });
 
     return injectors;
